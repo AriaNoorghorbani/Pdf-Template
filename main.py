5e8122ef6fec00783f25c42e67627054af75ec3c
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-16 12:12:16 +0300

    add footer

--- main.py
+++ main.py
@@ -7,11 +7,31 @@ read_csv = pd.read_csv("topics.csv")
 
 for index, row in read_csv.iterrows():
     pdf.add_page()
+    pdf.set_auto_page_break(auto=False)
+
+    #Set the header
     pdf.set_font("Times", "B", size=24)
+    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=30, h=12, txt=row["Topic"], align="L", ln=0 )
     pdf.line(10, 21, 200, 21)
+
+    #Set the footer
+    pdf.ln(275)
+    pdf.set_text_color(180,180,180)
+    pdf.line(12, 287,198, 287)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+
     for i in range(row["Pages"]-1):
         pdf.add_page()
+
+        # Set the footer
+        pdf.ln(275)
+        pdf.set_text_color(180, 180, 180)
+        pdf.line(12, 287, 198, 287)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 
 pdf.output("Output.pdf")
