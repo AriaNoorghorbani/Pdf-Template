e75f184d8e5963f8dbaa27db1898b5b2f2e1803e
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-16 12:28:54 +0300

    fix bug

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ for index, row in read_csv.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=30, h=12, txt=row["Topic"], align="L", ln=0 )
     pdf.line(10, 21, 200, 21)
+    pdf.line(10, 22, 200, 22)
 
+    #Add lines
     for i in range(33, 280, 10):
         pdf.line(10, i, 200, i)
 
     #Set the footer
     pdf.ln(275)
-    pdf.set_text_color(180,180,180)
     pdf.line(12, 287,198, 287)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
@@ -29,12 +30,23 @@ for index, row in read_csv.iterrows():
     for i in range(row["Pages"]-1):
         pdf.add_page()
 
+        #Set the header
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(100,100,100)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L")
+        pdf.line(10, 21, 200, 21)
+        pdf.line(10, 22, 200, 22)
+
         # Set the footer
         pdf.ln(275)
         pdf.set_text_color(180, 180, 180)
         pdf.line(12, 287, 198, 287)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # Add lines
+        for i in range(33, 280, 10):
+            pdf.line(10, i, 200, i)
 
 
 pdf.output("Output.pdf")
