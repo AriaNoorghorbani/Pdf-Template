5a6284cefafd4b7b0c1f066f4dc01590e16f0bb8
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-16 10:58:32 +0300

    Read data from csv file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
 
-pdf = FPDF(orientation="L", unit="mm", format="A4")
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+read_csv = pd.read_csv("topics.csv")
 
-pdf.set_font("Times", "B", size=12)
-
-pdf.cell(w=30, h=12, txt="Hello There", border=1, align="L", ln=0 )
-pdf.cell(w=0, h=12, txt="Hello There", border=1, align="L", ln=1 )
+for index, row in read_csv.iterrows():
+    pdf.add_page()
+    pdf.set_font("Times", "B", size=24)
+    pdf.cell(w=30, h=12, txt=row["Topic"], align="L", ln=0 )
+    pdf.line(10, 21, 200, 21)
 
 
 pdf.output("Output.pdf")
